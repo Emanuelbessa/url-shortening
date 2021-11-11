f14146bd6e0c131c4acf16b2f9f74707ce364a7b
Emanuel Bessa <41740085+Emanuelbessa@users.noreply.github.com>
2021-11-10 23:30:52 -0300

    feat: :sparkles: created Schema, DTO and interface

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UrlModule } from './url/url.module';
+import { UrlSchema } from './url/schemas/urlSchema';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGO_URI, { useNewUrlParser: true }),
+    MongooseModule.forFeature([{ name: 'urls', schema: UrlSchema }]),
     UrlModule,
   ],
   controllers: [AppController],

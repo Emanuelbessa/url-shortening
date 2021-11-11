446a30706c2d7e3fe819722e138a8761d9adaf7a
Emanuel Bessa <41740085+Emanuelbessa@users.noreply.github.com>
2021-11-10 23:15:40 -0300

    feat: :sparkles: created raw module, controller and service for URL

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UrlModule } from './url/url.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGO_URI, { useNewUrlParser: true }),
+    UrlModule,
   ],
   controllers: [AppController],
   providers: [AppService],
